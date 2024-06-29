76766946f3a372f7fcc05cbdf7cac7b1cf9db8c8
salahadin-dinsafa <salahadindinsafa2@gmail.com>
2024-06-29 10:09:35 +0300

    design/header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Header } from "@/components/home/Header";
+
 export default function Home() {
-	return <h1>Hello World!</h1>;
+	return (
+		<main>
+			<Header />
+		</main>
+	);
 }
---
+++ src/components/home/Header.tsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+	return (
+		<div className="bg-green-custom text-center shadow-lg">
+			<h1 className=" text-6xl pb-0 w-fi h-fit">Conduit</h1>
+			<h3>A place to share nextjs knowledge</h3>
+		</div>
+	);
+};
