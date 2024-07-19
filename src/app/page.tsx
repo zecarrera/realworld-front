4cfa9b4a0928701368f222b7c9c450af8a0df3fb
salahadin-dinsafa <salahadindinsafa2@gmail.com>
2024-07-19 09:14:59 +0300

    design/article header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 
+import { getSession } from "@/actions";
 import { Header } from "@/components/home/Header";
 import { TagList } from "@/components/home/TagList";
 import { ArticleList } from "@/components/home/ArticleList";
@@ -11,13 +12,25 @@ export const metadata: Metadata = {
 		"Conduit is realworld social blogging site. it uses a custom API for all requests, including authentication. home page of conduit",
 };
 
-export default function Home() {
+export default async function Home() {
+	const session = await getSession();
 	return (
 		<main>
 			<Header />
-			<div className="flex min-w-full py-2 px-4 md:px-10 lg:px-14">
+			<div className="flex min-w-full py-2 px-4 md:px-10 lg:px-14 mt-10">
 				<div className="flex-1">
-					<ArticleHeader />
+					<ArticleHeader
+						choices={[
+							{
+								name: "Your Feed",
+								query: `follow=${session.username}`,
+							},
+							{
+								name: "Global Feed",
+								query: ``,
+							},
+						]}
+					/>
 					<ArticleList />
 				</div>
 				<TagList />
--- src/components/home/ArticleHeader.tsx
+++ src/components/home/ArticleHeader.tsx
@@ -1,5 +1,43 @@
-export const ArticleHeader = () => {
-	return <div>
-		ArticleHeader
-	</div>;
+"use client";
+
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+
+import { Separator } from "@/components/ui/separator";
+import { cn } from "@/lib/utils";
+
+type TArticleHeader = {
+	choices: {
+		name: string;
+		query: string;
+	}[];
+};
+
+export const ArticleHeader: React.FC<TArticleHeader> = ({ choices }) => {
+	const searchParams = useSearchParams();
+	const search = searchParams.entries().next();
+	let query: string = search.value ? search.value.join("=") : "";
+
+	return (
+		<div>
+			<div className="flex gap-3 ">
+				{choices.map((choice, i) => (
+					<Link
+						className={cn(
+							"opacity-85 pb-2 hover:opacity-100 border-b border-b-white",
+
+							query === choice.query &&
+								" border-b-green-custom text-green-custom"
+						)}
+						key={i}
+						href={`/?${choice.query}`}
+						id=""
+					>
+						{choice.name}
+					</Link>
+				))}
+			</div>
+			<Separator />
+		</div>
+	);
 };
